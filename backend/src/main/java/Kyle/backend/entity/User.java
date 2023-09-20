28db9b5ab47a76be78df600ac3b60d3578fa3c50
kabradshaw1 <itllbok82@gmail.com>
2023-09-20 11:40:37 -0400

    working on user/auth routes in server

--- backend/src/main/java/Kyle/backend/entity/User.java
+++ backend/src/main/java/Kyle/backend/entity/User.java
@@ -2,14 +2,34 @@ package Kyle.backend.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
 
 @Entity
 @Table(name = "user")
 @Data
+@Getter
+@Setter
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerateType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    priviate Long id;
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date dateCreated;
 }
