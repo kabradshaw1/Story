a5253e4fbc5504a5e8e5fc7bd5ab6ababea1f5b2
kabradshaw1 <itllbok82@gmail.com>
2023-09-21 22:16:54 -0400

    working on permissions

--- backend/src/main/java/Kyle/backend/config/SecurityConfig.java
+++ backend/src/main/java/Kyle/backend/config/SecurityConfig.java
@@ -1,11 +1,22 @@
 package Kyle.backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+public class SecurityConfig extends WebSecurityConfiguration {
+  @Bean
+  public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    http
+      .csrf(csrf -> csrf.disable()) // Use the non-deprecated method
+      .authorizeHttpRequests(authorizeRequests ->
+        authorizeRequests
+          .antMatchers("/register").permitAll()
+          .anyRequest().authenticated()
+      );
+    return http.build();
+  }
 }
