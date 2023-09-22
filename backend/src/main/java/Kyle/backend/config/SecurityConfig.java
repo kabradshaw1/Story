0ed6c31a42873c820b029683d7c1449d34def239
kabradshaw1 <itllbok82@gmail.com>
2023-09-22 11:05:04 -0400

    working on the things

--- backend/src/main/java/Kyle/backend/config/SecurityConfig.java
+++ backend/src/main/java/Kyle/backend/config/SecurityConfig.java
@@ -3,18 +3,20 @@ package Kyle.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfiguration {
+@EnableWebSecurity
+public class SecurityConfig {
+
   @Bean
   public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
     http
-      .csrf(csrf -> csrf.disable()) // Use the non-deprecated method
+      .csrf(csrf -> csrf.disable())
       .authorizeHttpRequests(authorizeRequests ->
         authorizeRequests
-          .antMatchers("/register").permitAll()
+          .requestMatchers("/register").permitAll()
           .anyRequest().authenticated()
       );
     return http.build();
