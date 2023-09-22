cc20178a4103db9d7a7c29da3cfa90b21e75d179
kabradshaw1 <itllbok82@gmail.com>
2023-09-22 14:25:22 -0400

    register route currently getting 403

--- backend/src/main/java/Kyle/backend/controller/RegisterationController.java
+++ backend/src/main/java/Kyle/backend/controller/RegisterationController.java
@@ -2,6 +2,7 @@ package Kyle.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import Kyle.backend.service.UserService;
 import Kyle.backend.dto.RegisterRequest;
 
 @RestController
+@CrossOrigin("http://localhost:4200")
 public class RegisterationController {
 
   @Autowired
