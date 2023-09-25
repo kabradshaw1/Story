4347e16d4c7586fa00b137d6484a5abe1817263e
kabradshaw1 <itllbok82@gmail.com>
2023-09-25 16:44:10 -0400

    working on refresh token route

--- backend/src/main/java/Kyle/backend/controller/RefreshController.java
+++ backend/src/main/java/Kyle/backend/controller/RefreshController.java
@@ -1,11 +1,14 @@
 package Kyle.backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import Kyle.backend.dao.UserRepository;
 import Kyle.backend.dto.RefreshTokenRequest;
 import Kyle.backend.service.JwtService;
 
@@ -13,11 +16,16 @@ import Kyle.backend.service.JwtService;
 @CrossOrigin("http://localhost:4200")
 public class RefreshController {
 
-  // @PostMapping("/api/refresh")
-  // public ResponseEntity<String> refreshAccessToken(@RequestBody RefreshTokenRequest refreshTokenRequest) {
-  //   try {
-  //     String newAccessToken = JwtService.refreshAccessToken(refreshTokenRequest.getRefreshToken())
-  //   }
-  // }
+    @Autowired
+    private JwtService jwtService; // Inject JwtService
 
+    @PostMapping("/api/refresh")
+    public ResponseEntity<String> generateRefreshToken(@RequestBody RefreshTokenRequest refreshTokenRequest) {
+        try {
+            String newAccessToken = jwtService.generateRefreshToken(refreshTokenRequest.getRefreshToken());
+            return ResponseEntity.ok(newAccessToken); // Return the new access token
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid refresh token");
+        }
+    }
 }
--- backend/src/main/java/Kyle/backend/service/JwtService.java
+++ backend/src/main/java/Kyle/backend/service/JwtService.java
@@ -1,16 +1,23 @@
 package Kyle.backend.service;
 
 import java.util.Date;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
+import Kyle.backend.dao.UserRepository;
 import Kyle.backend.entity.User;
 
 @Service
 public class JwtService {
+
+  @Autowired
+  private UserRepository userRepository;
 
   private final String SECRET = "mySuperSecretKey";
   private final int ACCESS_TOKEN_EXPIRATION = 15 * 60 * 1000; // 15 minutes in milliseconds
@@ -26,14 +33,22 @@ public class JwtService {
       .sign(algorithm);
   }
 
-  public String generateRefreshToken(User user) {
-    Algorithm algorithm = Algorithm.HMAC256(SECRET);
-    return JWT.create()
-      .withClaim("username", user.getUsername())
-      .withClaim("email", user.getEmail())
-      .withIssuer("backend")
-      .withExpiresAt(new Date(System.currentTimeMillis() + REFRESH_TOKEN_EXPIRATION))
-      .sign(algorithm);
+  public String generateRefreshToken(String refreshToken) {
+    try {
+      DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC256(SECRET))
+        .build()
+        .verify(refreshToken);
+
+        String username = decodedJWT.getClaim("username").asString();
+        String email = decodedJWT.getClaim("email").asString();
+
+        Optional<User> userOptional = userRepository.findByEmail(email);
+
+        return generateRefreshToken(userOptional);
+    } catch (Exception e) {
+      // Handle various exceptions like TokenExpiredException, SignatureVerificationException, etc.
+      throw new RuntimeException("Invalid refresh token", e);
+    }
   }
 }
 
