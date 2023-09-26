c698b40b5cb1bcf30388ea4c062070be732c34c8
kabradshaw1 <itllbok82@gmail.com>
2023-09-26 11:01:42 -0400

    generateRefreshToken method is done

--- backend/src/main/java/Kyle/backend/controller/RefreshController.java
+++ backend/src/main/java/Kyle/backend/controller/RefreshController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import Kyle.backend.dao.UserRepository;
 import Kyle.backend.dto.RefreshTokenRequest;
 import Kyle.backend.service.JwtService;
 
--- backend/src/main/java/Kyle/backend/service/JwtService.java
+++ backend/src/main/java/Kyle/backend/service/JwtService.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
@@ -22,9 +24,10 @@ public class JwtService {
   private final String SECRET = "mySuperSecretKey";
   private final int ACCESS_TOKEN_EXPIRATION = 15 * 60 * 1000; // 15 minutes in milliseconds
   private final int REFRESH_TOKEN_EXPIRATION = 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
+  private Algorithm algorithm = Algorithm.HMAC256(SECRET);
 
   public String generateAccessToken(User user) {
-    Algorithm algorithm = Algorithm.HMAC256(SECRET);
+
     return JWT.create()
       .withClaim("username", user.getUsername())
       .withClaim("email", user.getEmail())
@@ -39,12 +42,21 @@ public class JwtService {
         .build()
         .verify(refreshToken);
 
-        String username = decodedJWT.getClaim("username").asString();
         String email = decodedJWT.getClaim("email").asString();
 
         Optional<User> userOptional = userRepository.findByEmail(email);
+         if(userOptional.isPresent()) {
+          User user = userOptional.get();
+          return JWT.create()
+            .withClaim("username", user.getUsername())
+            .withClaim("email", user.getEmail())
+            .withIssuer("backend")
+            .withExpiresAt(new Date(System.currentTimeMillis() + REFRESH_TOKEN_EXPIRATION))
+            .sign(algorithm);
+         } else {
+          return "User was not found";
+         }
 
-        return generateRefreshToken(userOptional);
     } catch (Exception e) {
       // Handle various exceptions like TokenExpiredException, SignatureVerificationException, etc.
       throw new RuntimeException("Invalid refresh token", e);
