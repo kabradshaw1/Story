037d2b2fda24611665f15055e7080da5892a7b09
kabradshaw1 <itllbok82@gmail.com>
2023-09-26 13:48:46 -0400

    changed refresh end point to look for token in header

--- backend/src/main/java/Kyle/backend/controller/RefreshController.java
+++ backend/src/main/java/Kyle/backend/controller/RefreshController.java
@@ -5,10 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
-import Kyle.backend.dto.RefreshTokenRequest;
 import Kyle.backend.service.JwtService;
 
 @RestController
@@ -19,9 +17,9 @@ public class RefreshController {
     private JwtService jwtService; // Inject JwtService
 
     @PostMapping("/api/refresh")
-    public ResponseEntity<?> refreshAccessToken(@RequestBody RefreshTokenRequest refreshTokenRequest) {
+    public ResponseEntity<?> refreshAccessToken(@RequestHeader(name = "X-Refresh-Token") String refreshToken) {
         try {
-            String newAccessToken = jwtService.refreshAccessToken(refreshTokenRequest.getRefreshToken());
+            String newAccessToken = jwtService.refreshAccessToken(refreshToken);
             return ResponseEntity.ok(newAccessToken); // Return the new access token
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid refresh token");
