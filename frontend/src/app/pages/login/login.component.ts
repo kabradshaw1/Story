887f11f63f93f80c9e9146f8d7c659c284874d64
kabradshaw1 <itllbok82@gmail.com>
2023-09-27 10:13:50 -0400

    working on login plage

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  message = '';
+  loading = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private store: Store
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(40)]]
+    });
+  }
+
+  onSubmit() {
+    this.loading = true;
+    const email = this.loginForm.get('email')?.value;
+    const password = this.loginForm.get('password')?.value;
+  }
 }
