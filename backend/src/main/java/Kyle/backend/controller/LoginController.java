7099dd44d5c0b76825013a8af39784a184dd9602
kabradshaw1 <itllbok82@gmail.com>
2023-09-28 10:56:37 -0400

    trying to set up refresh end point to get the refresh token from client cookie

--- backend/src/main/java/Kyle/backend/controller/LoginController.java
+++ backend/src/main/java/Kyle/backend/controller/LoginController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin( origins = "http://localhost:4200", allowCredentials = "true")
 public class LoginController {
 
   @Autowired
--- backend/src/main/java/Kyle/backend/controller/RefreshController.java
+++ backend/src/main/java/Kyle/backend/controller/RefreshController.java
@@ -8,21 +8,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import Kyle.backend.service.JwtService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin( origins = "http://localhost:4200", allowCredentials = "true")
 public class RefreshController {
 
     @Autowired
     private JwtService jwtService; // Inject JwtService
 
     @PostMapping("/api/refresh/")
-    public ResponseEntity<?> refreshAccessToken(@RequestHeader(name = "X-Refresh-Token") String refreshToken) {
-        try {
-            String newAccessToken = jwtService.refreshAccessToken(refreshToken);
-            return ResponseEntity.ok(newAccessToken); // Return the new access token
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid refresh token");
+    public ResponseEntity<?> refreshAccessToken(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                String[] nameValuePair = cookie.trim().split("=");
+                if (nameValuePair.length >= 2 && "refreshToken".equals(nameValuePair[0])) {
+                    String refreshToken = nameValuePair[1];
+                    try {
+                        String newAccessToken = jwtService.refreshAccessToken(refreshToken);
+                        return ResponseEntity.ok(newAccessToken);
+                    } catch (Exception e) {
+                        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid refresh token");
+                    }
+                }
+            }
         }
     }
 }
