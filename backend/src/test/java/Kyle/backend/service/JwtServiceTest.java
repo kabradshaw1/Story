c940966bea50b2eca7dcda95db6bc79f66d5ce16
kabradshaw1 <itllbok82@gmail.com>
2023-09-29 10:27:30 -0400

    working on tests

--- backend/src/test/java/Kyle/backend/service/JwtServiceTest.java
+++ backend/src/test/java/Kyle/backend/service/JwtServiceTest.java
@@ -1,14 +1,28 @@
 package Kyle.backend.service;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import Kyle.backend.dao.UserRepository;
+import Kyle.backend.entity.User;
 
 @ExtendWith(MockitoExtension.class)
 public class JwtServiceTest {
 
   @Mock
   private UserRepository userRepository;
+
+  @InjectMocks
+  private JwtService jwtService;
+
+  @Test
+  public void givenUser_whenGenerateAccessTken_thenReturnToken() {
+    // Given
+    User user = new User();
+    user.setUsername("testUser");
+    user.setEmail("test@email.com");
+  }
 }
