69e5723e52594daf9130717fc3e834bf53addbb2
kabradshaw1 <itllbok82@gmail.com>
2023-10-02 12:12:16 -0400

    working on tests

--- backend/src/test/java/Kyle/backend/controller/LoginControllerTest.java
+++ backend/src/test/java/Kyle/backend/controller/LoginControllerTest.java
@@ -2,7 +2,7 @@ package Kyle.backend.controller;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -56,10 +57,11 @@ public class LoginControllerTest {
 
     MvcResult result = mockMvc.perform(
       MockMvcRequestBuilders.post("/api/login/")
-        .cookie(new Cookie("refreshToken", dummyRefreshToken))
+      .contentType(MediaType.APPLICATION_JSON)
+      .content("{\"email\":\"" + email + "\", \"password\":\"" + password + "\"}")
     )
       .andExpect(status().isOk())
-      .andExpect(content().string(dummyAccessToken))
+      .andExpect(jsonPath("$.accessToken").value(dummyAccessToken))
       .andReturn();
 
     Cookie responseCookie = result.getResponse().getCookie("refreshToken");
