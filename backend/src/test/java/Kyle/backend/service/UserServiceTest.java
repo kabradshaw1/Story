c06899c63260523da49bf2a97e06a05d8c1eae69
kabradshaw1 <itllbok82@gmail.com>
2023-10-03 13:57:31 -0400

    working on the things

--- backend/src/test/java/Kyle/backend/service/UserServiceTest.java
+++ backend/src/test/java/Kyle/backend/service/UserServiceTest.java
@@ -1,5 +1,11 @@
 package Kyle.backend.service;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -7,6 +13,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import Kyle.backend.dao.UserRepository;
+import Kyle.backend.entity.User;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -17,8 +24,31 @@ public class UserServiceTest {
   @InjectMocks
   private UserService userService;
 
+  private User user;
+
+  @BeforeEach
+  private void setup() {
+    user = new User("testUser", "testPassword", "test@example.com");
+  }
+
   @Test
-  public void giveAcceptableCredentials_whenRegisterUser_thenCreateUser() {
+  public void givenAcceptableCredentials_whenRegisterUser_thenCreateUser() {
+    // Given
+    String username = "testUser1";
+    String password = "testPassword";
+    String email = "test1@example.com";
+
+    // Mocking behavior: assume the user with the given email and username does not exist
+    when(userRepository.findByEmail(email)).thenReturn(null);
+    when(userRepository.findByUsername(username)).thenReturn(null);
+
+    // When
+    userService.registerUser(username, password, email); // assuming your service has this method
+
+    // Then
+    // This assertion checks that the save method of the repository was called exactly once.
+    verify(userRepository, times(1)).save(any(User.class));
+
 
   }
 
