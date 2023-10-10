79948f373ffbf6ffc9456a89f16de6548b49b19d
kabradshaw1 <itllbok82@gmail.com>
2023-10-10 12:56:12 -0400

    working on the things

--- frontend/src/app/pages/login/login.component.spec.ts
+++ frontend/src/app/pages/login/login.component.spec.ts
@@ -9,16 +9,46 @@ describe('LoginComponent', () => {
   let fixture: ComponentFixture<LoginComponent>;
   let authServiceMock: jasmine.SpyObj<AuthService>
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [LoginComponent]
-    });
+  beforeEach(async () => {
+    authServiceMock = jasmine.createSpyObj('AuthService', ['login']);
+
+    await TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [LoginComponent],
+      providers: [{ provide: AuthService, useValue: authServiceMock }]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a button that triggers the login method', () => {
+    const button = fixture.debugElement.nativeElement.querySelector('button');
+    spyOn(component, 'onSubmit');
+    button.click();
+    expect(component.onSubmit).toHaveBeenCalled();
+  })
+
+  it('should validate email and password fields', () => {
+    it('should validate email and password fields', () => {
+      component.loginForm.controls['email'].setValue('test');
+      component.loginForm.controls['password'].setValue('');
+
+      expect(component.loginForm.controls['email'].valid).toBeFalse();
+      expect(component.loginForm.controls['password'].valid).toBeFalse();
+
+      component.loginForm.controls['email'].setValue('test@example.com');
+      component.loginForm.controls['password'].setValue('password');
+
+      expect(component.loginForm.controls['email'].valid).toBeTrue();
+      expect(component.loginForm.controls['password'].valid).toBeTrue();
+    });
+  });
+
+
 });
