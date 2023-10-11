598538c87f3adb391a0f4b3a11ae6c640d21f7ea
kabradshaw1 <itllbok82@gmail.com>
2023-10-11 13:29:51 -0400

    working on login component

--- frontend/src/app/pages/login/login.component.spec.ts
+++ frontend/src/app/pages/login/login.component.spec.ts
@@ -3,6 +3,7 @@ import { AuthService } from 'src/app/services/auth.service';
 import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { of, throwError } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -22,51 +23,156 @@ describe('LoginComponent', () => {
     component = fixture.componentInstance;
   });
 
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have a button that triggers the login method', () => {
-    const button = fixture.debugElement.nativeElement.querySelector('button');
-    spyOn(component, 'onSubmit');
-    button.click();
-    expect(component.onSubmit).toHaveBeenCalled();
-  })
+  describe('Validation', () => {
 
-  it('should validate email and password fields', () => {
-    component.loginForm.controls['email'].setValue('test');
-    component.loginForm.controls['password'].setValue('');
+    it('should validate email and password fields', () => {
+      component.loginForm.controls['email'].setValue('test');
+      component.loginForm.controls['password'].setValue('');
 
-    expect(component.loginForm.controls['email'].valid).toBeFalse();
-    expect(component.loginForm.controls['password'].valid).toBeFalse();
+      expect(component.loginForm.controls['email'].valid).toBeFalse();
+      expect(component.loginForm.controls['password'].valid).toBeFalse();
 
-    component.loginForm.controls['email'].setValue('test@example.com');
-    component.loginForm.controls['password'].setValue('password');
+      component.loginForm.controls['email'].setValue('test@example.com');
+      component.loginForm.controls['password'].setValue('password');
 
-    expect(component.loginForm.controls['email'].valid).toBeTrue();
-    expect(component.loginForm.controls['password'].valid).toBeTrue();
+      expect(component.loginForm.controls['email'].valid).toBeTrue();
+      expect(component.loginForm.controls['password'].valid).toBeTrue();
+    });
+
+    it('should display an error message on failed login', () => {
+      component.loginForm.controls['email'].setValue('test@example.com');
+      component.loginForm.controls['password'].setValue('password');
+      authServiceMock.login.and.returnValue(throwError(() => { error: 'Invalid credentials' }));
+
+      component.onSubmit();
+      fixture.detectChanges();
+      const errorMessage = fixture.debugElement.nativeElement.querySelector('.error-message');
+      expect(errorMessage.textContent).toContain('Invalid credentials');
+    });
+
+    it('should display a required error message when email is empty', () => {
+      component.loginForm.controls['email'].setValue('');
+      component.loginForm.controls['email'].markAsTouched();
+      fixture.detectChanges();
+
+      const emailError = fixture.debugElement.nativeElement.querySelector('.email-error');
+      expect(emailError.textContent).toContain('Email is required');
+    });
+
+    it('should display an invalid email format error message when email format is incorrect', () => {
+      component.loginForm.controls['email'].setValue('invalidEmailFormat');
+      component.loginForm.controls['email'].markAsTouched();
+      fixture.detectChanges();
+
+      const emailError = fixture.debugElement.nativeElement.querySelector('.email-error-format');
+      expect(emailError.textContent).toContain('Please enter a valid email');
+    });
+
+    it('should display an invalid password error message when password is less then 8 characters', () => {
+      component.loginForm.controls['password'].setValue('passwor');
+      component.loginForm.controls['password'].markAsTouched();
+      fixture.detectChanges();
+
+      const passwordError = fixture.debugElement.nativeElement.querySelector('.password-error-format');
+      expect(passwordError.textContent).toContain('This password is too short')
+    });
+
+    it('should reject invalid email formats', () => {
+      const invalidEmails = [
+        "plainaddress",
+        "@missingusername.org",
+        "username@.com",
+        "username@domain.com.",
+        "username@.domain.com",
+        ".username@domain.com"
+        // Add other invalid formats as required
+      ];
+
+      for (let email of invalidEmails) {
+        component.loginForm.controls['email'].setValue(email);
+
+        // Check if email form control is invalid
+        expect(component.loginForm.controls['email'].valid).withContext(`Expected '${email}' to be invalid.`).toBeFalse();
+      }
+    });
   });
 
+  describe('on form submit', () => {
 
-  it('should call AuthService login method on valid form submission', () => {
-    component.loginForm.controls['email'].setValue('test@example.com');
-    component.loginForm.controls['password'].setValue('password');
-    authServiceMock.login.and.returnValue(of({ accessToken: 'mock-token' }));
+    it('should use the input from the form fields when the button is clicked', () => {
+      // Find the input elements using DebugElement
+      const emailInput = fixture.debugElement.query(By.css('input[type="email"]')).nativeElement;
+      const passwordInput = fixture.debugElement.query(By.css('input[type="password"]')).nativeElement;
 
-    component.onSubmit();
-    expect(authServiceMock.login).toHaveBeenCalledWith('test@example.com', 'password');
+      // Simulate user typing into the input elements
+      emailInput.value = 'test@example.com';
+      passwordInput.value = 'testpassword';
+
+      // Dispatch input events to simulate user typing
+      emailInput.dispatchEvent(new Event('input'));
+      passwordInput.dispatchEvent(new Event('input'));
+
+      // Trigger change detection to process the bindings
+      fixture.detectChanges();
+
+      // Trigger the button click
+      const button = fixture.debugElement.nativeElement.querySelector('button');
+      button.click();
+
+      // Assert that AuthService's login method was called with the form field values
+      expect(authServiceMock.login).toHaveBeenCalledWith('test@example.com', 'testpassword');
+    });
+
+    it('should show a loading indicator when logging in', () => {
+      component.loading = true;
+      fixture.detectChanges();
+      const spinner = fixture.debugElement.nativeElement.querySelector('.loading-spinner');
+      expect(spinner).toBeTruthy();
+    });
+
+    it('should hide loading indicator if login fails', () => {
+      // Arrange: Set the login method of AuthService to return an error.
+      authServiceMock.login.and.returnValue(throwError(() => new Error('Invalid credentials')));
+
+      // Assume you have a loading property in your component which shows/hides the spinner.
+      // Set it to true initially to simulate the start of the login process.
+      component.loading = true;
+
+      // Act: Trigger the login action.
+      component.onSubmit();
+
+      // Assert: After the login action fails, the loading spinner should not be visible.
+      fixture.detectChanges();
+      const spinner = fixture.debugElement.nativeElement.querySelector('.loading-spinner');
+      expect(spinner).toBeNull();
+      expect(component.loading).toBeFalse(); // If you maintain a 'loading' flag in your component.
   });
 
-  it('should display an error message on failed login', () => {
-    component.loginForm.controls['email'].setValue('test@example.com');
-    component.loginForm.controls['password'].setValue('password');
-    authServiceMock.login.and.returnValue(throwError({ error: 'Invalid credentials' }));
-
-    component.onSubmit();
-    fixture.detectChanges();
-    const errorMessage = fixture.debugElement.nativeElement.querySelector('.error-message');
-    expect(errorMessage.textContent).toContain('Invalid credentials');
   });
 
+  describe('Normal state', () => {
+    it('should enable the submit button if the form is valid', () => {
+      component.loginForm.controls['email'].setValue('test@example.com');
+      component.loginForm.controls['password'].setValue('password123');
+      fixture.detectChanges();
+      const button = fixture.debugElement.nativeElement.querySelector('button');
+      expect(button.disabled).toBeFalse();
+    });
+
+    it('should have an invalid form on init', () => {
+      expect(component.loginForm.valid).toBeFalse();
+    });
+
+    it('should disable the submit button if the form is invalid', () => {
+      component.loginForm.controls['email'].setValue('');
+      component.loginForm.controls['password'].setValue('');
+      fixture.detectChanges();
+      const button = fixture.debugElement.nativeElement.querySelector('button');
+      expect(button.disabled).toBeTrue();
+    });
+  });
 });
--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { Store } from '@ngrx/store';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -15,8 +14,8 @@ export class LoginComponent {
 
   constructor(
     private fb: FormBuilder,
-    // private router: Router,
-    // private store: Store
+    private authService: AuthService
+
   ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
@@ -24,9 +23,28 @@ export class LoginComponent {
     });
   }
 
-  onSubmit() {
+onSubmit() {
     this.loading = true;
     const email = this.loginForm.get('email')?.value;
     const password = this.loginForm.get('password')?.value;
-  }
+
+    this.authService.login(email, password).subscribe({
+      next: (response) => {
+        // Handle successful login here, e.g., navigate to another route or set user data
+        this.message = 'Login successful';
+      },
+      error: (err) => {
+        // Handle errors here based on the error response
+        this.loading = false; // Hide loading spinner
+
+        // You can have more specific error messages based on the type of error
+        if (err?.error?.message === 'Invalid credentials') {
+          this.message = 'Invalid Credentials';
+        } else {
+          this.message = 'An error occurred during login. Please try again.';
+        }
+      }
+    });
+}
+
 }
