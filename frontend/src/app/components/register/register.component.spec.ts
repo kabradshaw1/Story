3a8e3cc1ea08af918481302820b9c86a43959893
kabradshaw1 <itllbok82@gmail.com>
2023-10-17 13:32:51 -0400

    starting on Register component

--- frontend/src/app/components/register/register.component.spec.ts
+++ frontend/src/app/components/register/register.component.spec.ts
@@ -1,18 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import { LoginComponent } from '../login/login.component';
+import { AuthService } from 'src/app/services/auth.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let authServiceMock: jasmine.SpyObj<AuthService>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [RegisterComponent]
-    });
+  beforeEach(async () => {
+    authServiceMock = jasmine.createSpyObj('AuthService', ['register']);
+
+    await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      declarations: [RegisterComponent],
+      providers: [{ provide: AuthService, useValue: authServiceMock }]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RegisterComponent);
-    component = fixture.componentInstance;
     fixture.detectChanges();
+    component = fixture.componentInstance;
+  });
+
+  afterEach(() => {
+    component.loading = false; // Reset the loading state
+    component.message = ''; // Reset any messages
   });
 
   it('should create', () => {
--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService
+  ) {
+      this.registerForm = this.fb.group({
 
+      })
+    }
+  onSubmit(): Promise<void> {
+    return new Promise((resolve) => {
+
+    })
+  }
 }
