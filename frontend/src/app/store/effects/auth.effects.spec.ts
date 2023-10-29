848b91c86af234ed5b1c0aa22c9fcb643a4f7c72
kabradshaw1 <itllbok82@gmail.com>
2023-10-29 14:09:43 -0400

    working on auth.effects

--- frontend/src/app/store/effects/auth.effects.spec.ts
+++ frontend/src/app/store/effects/auth.effects.spec.ts
@@ -3,7 +3,7 @@ import { provideMockActions } from '@ngrx/effects/testing'
 import { Action } from "@ngrx/store";
 import { Observable, of, throwError } from "rxjs";
 import { AuthEffects } from "./auth.effects";
-import * as AuthAction from '../actions/auth.actions';
+import * as AuthActions from '../actions/auth.actions';
 import { AuthService } from "src/app/services/auth.service";
 import { cold, hot } from 'jasmine-marbles';
 
@@ -23,5 +23,23 @@ describe('AuthEffect', () => {
         }
       ]
     });
+    effects = TestBed.inject(AuthEffects);
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>
   });
+
+  describe('login$', () => {
+    it('givenLoginAction_whenServiceSucceeds_thenDispatchAuthService', () => {
+      const credentials = { email: 'test@email.com', password: 'password123' };
+      const action = AuthActions.login(credentials);
+      const completion = AuthActions.authSuccess({ accessToken: 'testToken' });
+
+      actions$ = hot('-a', { a: action });
+      authService.login.and.returnValue(cold('-b', { b: { accessToken: 'testToken' } }));
+
+      const expected = cold('--c', { c: completion });
+
+      expect(effects.login$).toBeObservable(expected);
+    });
+  })
+  // authService.login.and.returnValue(throwError(() => Error('Failed')));
 });
