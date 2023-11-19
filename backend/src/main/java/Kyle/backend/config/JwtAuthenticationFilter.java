ba95646174d293f7ca5173b6416712119066a6f0
kabradshaw1 <itllbok82@gmail.com>
2023-11-19 10:38:46 -0500

    working on security

--- backend/src/main/java/Kyle/backend/config/JwtAuthenticationFilter.java
+++ backend/src/main/java/Kyle/backend/config/JwtAuthenticationFilter.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import Kyle.backend.service.JwtService;
 import jakarta.servlet.FilterChain;
@@ -12,7 +14,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class JwtAuthenticationFilter {
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   @Autowired
   private JwtService jwtService;
