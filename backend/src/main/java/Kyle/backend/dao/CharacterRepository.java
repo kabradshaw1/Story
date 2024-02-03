f33256edaf152ba9ccbe7ede9d39522990726ba3
kabradshaw1 <itllbok82@gmail.com>
2024-02-03 10:23:58 -0500

    working on the things

---
+++ backend/src/main/java/Kyle/backend/dao/CharacterRepository.java
@@ -0,0 +1,13 @@
+package Kyle.backend.dao;
+
+import Kyle.backend.entity.Character;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface CharacterRepository extends JpaRepository<Character, Long> {
+  Optional<Character> findByName(Long name);
+}
--- backend/src/test/java/Kyle/backend/controller/CharacterControllerTest.java
+++ backend/src/test/java/Kyle/backend/controller/CharacterControllerTest.java
@@ -5,9 +5,11 @@ import static org.mockito.Mockito.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import Kyle.backend.entity.Character;
+import Kyle.backend.service.CharacterService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -15,4 +17,7 @@ public class CharacterControllerTest {
 
   @Autowired
   private MockMvc mockMvc;
+
+  @MockBean
+  private CharacterService characterService;
 }
