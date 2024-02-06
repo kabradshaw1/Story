5372cecfa69672b508b3e1cdf8bddb5bdbd5049e
kabradshaw1 <itllbok82@gmail.com>
2024-02-06 17:13:45 -0500

    working on server, server broken

--- backend/src/main/java/Kyle/backend/service/CharacterService.java
+++ backend/src/main/java/Kyle/backend/service/CharacterService.java
@@ -1,5 +1,15 @@
 package Kyle.backend.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import Kyle.backend.dao.CharacterRepository;
+
 public class CharacterService {
-  
+
+  @Autowired
+  private CharacterRepository characterRepository;
+
+  public Character findByName(String name) {
+
+  }
 }
--- backend/src/test/java/Kyle/backend/service/CharacterServiceTest.java
+++ backend/src/test/java/Kyle/backend/service/CharacterServiceTest.java
@@ -1,6 +1,12 @@
 package Kyle.backend.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +17,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import Kyle.backend.dao.CharacterRepository;
 import Kyle.backend.entity.Character;
+import Kyle.backend.entity.Scene;
 
 @ExtendWith(MockitoExtension.class)
 public class CharacterServiceTest {
@@ -25,15 +32,27 @@ public class CharacterServiceTest {
   @BeforeEach
   private void setup() {
     character = new Character("testName", "testBio");
+    Set<Scene> scenes = new HashSet<>();
+    Scene scene = new Scene();
+    scene.setTitle("Scene 1");
+    scenes.add(scene);
+
+    character.setScenes(scenes);
   }
 
   @Test
   public void givenCharacterExists_whenCharacterSearched_thenReturnCharacter() {
     // Given
     String name = "testName";
+    when(characterRepository.findByName(name)).thenReturn(Optional.of(character));
 
     // When
-    when(null);
+    Character foundCharacter = characterService.findByName(name);
+
     // Then
+    assertNotNull(foundCharacter, "The found character should not be null.");
+    assertEquals(name, foundCharacter.getName(), "The name of the found character should match the search query.");
+    assertNotNull(foundCharacter.getScenes(), "The character should have associated scenes.");
+    assertEquals(1, foundCharacter.getScenes().size(), "The character should have one associated scene.");
   }
 }
