c45f7a2c6f7f82fa491f26e0a29dba015728a0c3
Kyle Bradshaw <kylebradshaw@Kyles-Air.attlocal.net>
2024-02-14 15:17:02 -0500

    working on security

--- backend/src/main/java/Kyle/backend/dao/CharacterRepository.java
+++ backend/src/main/java/Kyle/backend/dao/CharacterRepository.java
@@ -5,9 +5,18 @@ import Kyle.backend.entity.Character;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
+@RepositoryRestResource
 public interface CharacterRepository extends JpaRepository<Character, Long> {
+
+  @SuppressWarnings("null")
+  @Override
+  @PreAuthorize("hasRole('Admin') or #entity.createdBy == authentication.hame")
+  <S extends Character> S save(S entity);
+  
   Optional<Character> name(String name);
 }
