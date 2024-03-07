965f81ab5890137cc8ed9b2779e84860c9db6735
kabradshaw1 <itllbok82@gmail.com>
2024-03-06 21:29:10 -0500

    working on protecting deletes

--- backend/src/test/java/Kyle/backend/controller/CharacterControllerTest.java
+++ backend/src/test/java/Kyle/backend/controller/CharacterControllerTest.java
@@ -1,8 +1,10 @@
 package Kyle.backend.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -11,6 +13,7 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -23,6 +26,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import Kyle.backend.config.CustomUserPrincipal;
 import Kyle.backend.dao.CharacterRepository;
+import Kyle.backend.entity.Character;
 import Kyle.backend.service.JwtService;
 
 @SpringBootTest
@@ -41,7 +45,7 @@ public class CharacterControllerTest {
 
   @SuppressWarnings("null")
   @Test
-  public void given_when_then() throws Exception {
+  public void givenAuthUser_whenCharacterPost_thenCreatePost() throws Exception {
     when(jwtService.validateToken("valid.token")).thenReturn(true);
 
     String body = """
@@ -62,7 +66,6 @@ public class CharacterControllerTest {
       null,
       Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
     );
-
     when(jwtService.getAuthentication("valid.token")).thenReturn(authentication);
 
     mockMvc.perform(post("/api/characters")
@@ -76,5 +79,37 @@ public class CharacterControllerTest {
     assertTrue(optionalCharacter.isPresent());
     assertEquals("string", optionalCharacter.get().getBio());
     assertEquals("username", optionalCharacter.get().getUsername());
+  }
+
+  @Test
+  public void givenWrongAuthUser_whenDeletingPost_thenReturnError() throws Exception {
+    // Mocks making it past the JwtAuthFilter
+    when(jwtService.validateToken("valid.token")).thenReturn(true);
+
+    // Create a character in the mocked database.  This is all I need to locate a character
+    // and verify that it was made by the person that is authenticated
+    Character existingCharacter = new Character();
+    existingCharacter.setId(1L);
+    existingCharacter.setUsername("Tester");
+    Character savedCharacter = characterRepository.save(existingCharacter);
+    assertNotNull(savedCharacter.getId());
+
+    // Mocks use of JwtService.getAuth()
+    CustomUserPrincipal principal = new CustomUserPrincipal(
+      "WrongUsername",
+      1L,
+      Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
+    );
+    Authentication authentication = new UsernamePasswordAuthenticationToken(
+      principal,
+      null,
+      Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
+    );
+    when(jwtService.getAuthentication("valid.token")).thenReturn(authentication);
+
+    // Test that delete doesn't work
+    mockMvc.perform(delete("/api/characters/1")
+      .header("Authorization", "Bearer valid.token"))
+      .andExpect(status().isForbidden());
   }
 }
