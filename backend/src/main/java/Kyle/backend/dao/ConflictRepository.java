d76b946b47bae5a41defc10b80c88c6b1c23b19e
kabradshaw1 <itllbok82@gmail.com>
2024-03-13 14:45:39 -0400

    stuff and things

---
+++ backend/src/main/java/Kyle/backend/dao/ConflictRepository.java
@@ -0,0 +1,22 @@
+// package Kyle.backend.dao;
+
+// import Kyle.backend.entity.Conflict;
+
+// import java.util.Optional;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+// import org.springframework.security.access.prepost.PreAuthorize;
+// import org.springframework.web.bind.annotation.CrossOrigin;
+
+// @CrossOrigin("http://localhost:4200")
+// @RepositoryRestResource
+// public interface ConflictRepository extends JpaRepository<Conflict, Long> {
+
+//   @SuppressWarnings("null")
+//   @Override
+//   @PreAuthorize("hasRole('Admin') or #entity.username == authentication.principal.username")
+//   <S extends Conflict> S save(S entity);
+
+//   Optional<Conflict> title(String title);
+// }
---
+++ backend/src/main/java/Kyle/backend/dao/OrganizationRepository.java
@@ -0,0 +1,22 @@
+// package Kyle.backend.dao;
+
+// import Kyle.backend.entity.Organization;
+
+// import java.util.Optional;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+// import org.springframework.security.access.prepost.PreAuthorize;
+// import org.springframework.web.bind.annotation.CrossOrigin;
+
+// @CrossOrigin("http://localhost:4200")
+// @RepositoryRestResource
+// public interface OrganizationRepository extends JpaRepository<Organization, Long> {
+
+//   @SuppressWarnings("null")
+//   @Override
+//   @PreAuthorize("hasRole('Admin') or #entity.username == authentication.principal.username")
+//   <S extends Organization> S save(S entity);
+
+//   Optional<Organization> title(String title);
+// }
