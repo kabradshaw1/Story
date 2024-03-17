7d7230b14da47225a3adee2c28fd42f042eba59e
kabradshaw1 <itllbok82@gmail.com>
2024-03-17 19:05:54 -0400

    working on tests

--- frontend/src/app/components/nav-menu/nav-menu.component.spec.ts
+++ frontend/src/app/components/nav-menu/nav-menu.component.spec.ts
@@ -1,32 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavMenuComponent } from './nav-menu.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 describe('NavMenuComponent', () => {
   let component: NavMenuComponent;
   let fixture: ComponentFixture<NavMenuComponent>;
+  let router: Router;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
       declarations: [NavMenuComponent],
       imports: [RouterTestingModule],
 
-    });
+    }).compileComponents();
+
+    router = TestBed.inject(Router);
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(NavMenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
   describe('html', () => {
+   // Updated to match the button's routerLink value
     it('give_whenComponentLoaded_thenDisplayButtons', () => {
-      const fixture = TestBed.createComponent(NavMenuComponent);
-      fixture.detectChanges();
-      const compiled = fixture.nativeElement as HTMLElement;
-      expect(compiled.querySelector('a[href="characters"]')).toBeTruthy();
-    })
+      const navigateSpy = spyOn(router, 'navigate');
+      const button = fixture.debugElement.nativeElement.querySelector('#nav-characters');
+      button.click();
+      expect(navigateSpy).toHaveBeenCalledWith(['/characters']);
+    });
   })
 });
