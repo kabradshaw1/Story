b71217afc51cd96f41eba14dfd5ffb10a86e484a
kabradshaw1 <itllbok82@gmail.com>
2024-03-17 11:17:01 -0400

    working on nav-menu frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProtectedComponent } from './components/protected/protected.component';
 import { EffectsModule } from '@ngrx/effects';
 import { AuthEffects } from './store/effects/auth.effects';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+
 @NgModule({
   declarations: [
     AppComponent,
