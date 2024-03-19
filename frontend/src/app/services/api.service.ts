6d338df58b83894b2cc357b20e3d27a1110a3ce6
kabradshaw1 <itllbok82@gmail.com>
2024-03-18 21:35:49 -0400

    working on frontend

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -1,3 +1,12 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { ApiState } from "../store/state/api.state";
+
+@Injectable({providedIn: 'root'})
 export class ApiService {
-  
+  constructor(
+    private http: HttpClient,
+    private store: Store<ApiState> 
+  ) {}
 }
