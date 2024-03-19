5ab658677990bdbd13746a615fd2fbbc490765ff
kabradshaw1 <itllbok82@gmail.com>
2024-03-19 16:37:17 -0400

    working on reducer

--- frontend/src/app/store/reducers/api.reducers.spec.ts
+++ frontend/src/app/store/reducers/api.reducers.spec.ts
@@ -3,5 +3,44 @@ import * as ApiActions from '../actions/api.actions';
 import { apiReducer } from "./api.reducers";
 
 describe('Api Reducer', () => {
-  
+  it('givenUnknownAction_whenReducerCalled_thenNoStateChange', () => {
+    const action = { type: 'UNKNOWN' };
+    const state = apiReducer(initialApiState, action);
+
+    expect(state).toBe(initialApiState);
+  });
+  it('givenSuccess_whenReducerCalled_thenUpdateState', () => {
+    
+    const initialStateWithError: ApiState = {
+      character: null,
+      scene: null,
+      location: null,
+      region: null,
+      conflict: null,
+      organization: null,
+      timeline: null,
+      error: {endpoint:'character', message:'some inital error'}
+    };
+
+    const action = ApiActions.apiSuccess(
+      {
+        endpoint: 'character', 
+        data:{ 
+          title: 'test title', 
+          body: 'test body' 
+        }
+      }
+    );
+    const state = apiReducer(initialStateWithError, action);
+
+    expect(state.character?.body).toBe('test body');
+    expect(state.character?.title).toBe('test title');
+    expect(state.error).toBe(null); // Check that error is reset to null
+  });
+  it('given_when_then', () => {
+
+  });
+  it('given_when_then', () => {
+
+  });
 })
