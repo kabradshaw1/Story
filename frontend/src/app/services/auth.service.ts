2e307ff4f2122e06378f415004904989b9459188
kabradshaw1 <itllbok82@gmail.com>
2024-03-20 16:27:39 -0400

    refactored auth

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Store } from '@ngrx/store';
-import { Observable, catchError, tap, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 import { environment } from '../environment/environment';
-import { AuthResponse, SuccessResponse } from '../types';
-import AppState from '../store/state/app.state';
+import { AuthResponse } from '../types';
 
 @Injectable({
   providedIn: 'root'
@@ -18,68 +16,17 @@ export class AuthService {
 
   constructor(
     private http: HttpClient,
-    private store: Store<AppState>
   ) { }
 
   login(email: string, password: string): Observable<AuthResponse> {
-    const credentials = { email, password };
-    return this.http.post<AuthResponse>(this.loginUrl, credentials)
-      .pipe(
-        tap(response => {
-          if (isSuccessResponse(response)) {
-            this.store.dispatch({
-              type: '[Auth] Success',
-              payload: response.accessToken
-            });
-          }
-        }),
-        catchError(error => {
-          this.store.dispatch({ type: '[Auth] Failure', payload: error.statusText });
-          return throwError(() => error);
-        })
-      );
-  };
-
+    return this.http.post<AuthResponse>(this.loginUrl, { email, password });
+  }
+  
   register(email: string, password: string, username: string): Observable<AuthResponse> {
-    const credentials = { email, password, username };
-
-    return this.http.post<AuthResponse>(this.registerUrl, credentials)
-      .pipe(
-        tap(response => {
-          console.log(credentials)
-          if (isSuccessResponse(response)) {
-            this.store.dispatch({
-              type: '[Auth] Success',
-              payload: response.accessToken
-            });
-          }
-        }),
-        catchError(error => {
-          this.store.dispatch({ type: '[Auth] Failure', payload: error.statusText });
-          return throwError(() => error);
-        })
-      );
+    return this.http.post<AuthResponse>(this.registerUrl, { email, password, username });
   }
-
+  
   refresh(): Observable<AuthResponse> {
-    return this.http.post<AuthResponse>(this.refreshUrl, {})
-      .pipe(
-        tap(response => {
-          if (isSuccessResponse(response))
-            this.store.dispatch({
-              type: '[Auth] Success',
-              payload: response.accessToken
-            })
-        }),
-        catchError(error => {
-          this.store.dispatch({ type: '[Auth] Failure', payload: error.statusText });
-          return throwError(() => error);
-        })
-      )
+    return this.http.post<AuthResponse>(this.refreshUrl, {});
   }
 }
-
-function isSuccessResponse(response: AuthResponse): response is SuccessResponse {
-  return (response as SuccessResponse).accessToken !== undefined;
-}
-
--- frontend/src/app/store/effects/auth.effects.ts
+++ frontend/src/app/store/effects/auth.effects.ts
@@ -1,14 +1,13 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import * as AuthActions from '../actions/auth.actions';
-import { Observable, catchError, map, mergeMap, of, tap } from "rxjs";
+import { catchError, map, mergeMap, of } from "rxjs";
 import { AuthService } from "src/app/services/auth.service";
 import { AuthResponse } from "src/app/types";
 import { Router } from "@angular/router";
 
 @Injectable()
 export class AuthEffects {
-
   constructor(
     private actions$: Actions,
     private authService: AuthService,
@@ -18,10 +17,10 @@ export class AuthEffects {
   login$ = createEffect(() =>
     this.actions$.pipe(
       ofType(AuthActions.login),
-      mergeMap(action =>
+      mergeMap((action) =>
         this.authService.login(action.email, action.password).pipe(
-          this.handleAuthResponse(),
-          catchError(this.handleError())
+          map(response => this.handleAuthResponse(response)),
+          catchError(error => of(this.handleError(error)))
         )
       )
     )
@@ -30,11 +29,10 @@ export class AuthEffects {
   register$ = createEffect(() =>
     this.actions$.pipe(
       ofType(AuthActions.register),
-      mergeMap(action =>
+      mergeMap((action) =>
         this.authService.register(action.email, action.password, action.username).pipe(
-          tap(response=> console.log('Register response:', response)),
-          this.handleAuthResponse(),
-          catchError(this.handleError())
+          map(response => this.handleAuthResponse(response)),
+          catchError(error => of(this.handleError(error)))
         )
       )
     )
@@ -45,44 +43,25 @@ export class AuthEffects {
       ofType(AuthActions.refresh),
       mergeMap(() =>
         this.authService.refresh().pipe(
-          map((response: AuthResponse) => {
-            if ('accessToken' in response) {
-                return AuthActions.authSuccess({ accessToken: response.accessToken });
-            } else if ('error' in response) {
-                this.router.navigate(['/login']);
-                return AuthActions.authFailure({ error: response.error });
-            } else {
-                this.router.navigate(['/login']);
-                return AuthActions.authFailure({ error: "An unknown error occurred. Please try again." });
-            }
-          }),
-          catchError(error => {
-            this.router.navigate(['/login']);
-            return of(AuthActions.authFailure({ error: error.message || 'Error occurred while refreshing.' }));
-          })
+          map(response => this.handleAuthResponse(response)),
+          catchError(error => of(this.handleError(error)))
         )
       )
     )
-  )
+  );
 
-  private handleAuthResponse() {
-    return (source$: Observable<AuthResponse>) => source$.pipe(
-      map(response => {
-        if ('accessToken' in response) {
-          this.router.navigate(['/']);
-          return AuthActions.authSuccess({ accessToken: response.accessToken });
-        } else if ('error' in response) {
-          return AuthActions.authFailure({ error: response.error });
-        } else {
-          return AuthActions.authFailure({ error: "An unknown error occurred. Please try again." });
-        }
-      })
-    );
+  private handleAuthResponse(response: AuthResponse) {
+    if ('accessToken' in response) {
+      this.router.navigate(['/']);
+      return AuthActions.authSuccess({ accessToken: response.accessToken });
+    } else {
+      this.router.navigate(['/login']);
+      return AuthActions.authFailure({ error: response.error || "An unknown error occurred. Please try again." });
+    }
   }
 
-  private handleError() {
-    return (error: any) => {
-        return of(AuthActions.authFailure({ error: error.message || "An unknown error occurred." }));
-    };
+  private handleError(error: any) {
+    this.router.navigate(['/login']);
+    return AuthActions.authFailure({ error: error.message || "An unknown error occurred." });
   }
 }
